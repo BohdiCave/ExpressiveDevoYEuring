240c8f7d89825aee0434ec5ae444836d9e912e31
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2020-11-24 08:22:21 -0500

    preparation for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
+const env = require("custom-env").env();
 
-const connection = mysql.createConnection ({
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "gourmet_food_db"
 });
+}
 
 connection.connect(function(err) {
   if (err) {
