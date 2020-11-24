e186b06a50dea83585e1e13ed7c1352cb37a6bcf
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2020-11-24 11:32:55 -0500

    minor changes to the footer

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection ({
     host: "localhost",
-    port: 3306,
+    port: 8080,
     user: "root",
     password: "",
     database: "gourmet_food_db"
